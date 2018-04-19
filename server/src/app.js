62453efea90b4a1fd85cc4ce91f421701fa7a8a5
Nathan Schulzke <nschulzke@gmail.com>
2018-04-18 22:44:19 -0600

    Enable https and redirect

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,23 @@
 // Express Setup
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const expressWs = require('express-ws')(app);
 let PORT = process.env.SERVER_PORT;
+
+const credentials = {
+  cert: fs.readFileSync('./ssl/fullchain.pem'),
+  key: fs.readFileSync('./ssl/privkey.pem')
+};
+
+var http = express();
+http.get('*', function(req, res) {
+  res.redirect('https://' + req.headers.host + req.url);
+});
+http.listen(3080,
+  () => console.log('Server redirecting on port 3080!'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -12,6 +26,11 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log('Server listening on port ' + PORT + '!'));
+http.get("*", function(req, res, next) {
+  res.redirect("https://" + req.headers.host + "/" + req.path);
+});
+
+https.createServer(credentials, app).listen(PORT,
+  () => console.log('Server listening on port ' + PORT + '!'));
 
 module.exports = app;
