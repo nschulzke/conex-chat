57bdd8cdfc21c45387597d5284325944df7e8c32
Nathan Schulzke <nschulzke@gmail.com>
2018-04-20 14:16:55 -0600

    Make server run on not https in dev

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 // Express Setup
+const env = process.env.NODE_ENV || 'development';
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
@@ -7,30 +8,35 @@ const app = express();
 const expressWs = require('express-ws')(app);
 let PORT = process.env.SERVER_PORT;
 
-const credentials = {
-  cert: fs.readFileSync('./ssl/fullchain.pem'),
-  key: fs.readFileSync('./ssl/privkey.pem')
-};
-
-var http = express();
-http.get('*', function(req, res) {
-  res.redirect('https://' + req.headers.host + req.url);
-});
-http.listen(3080,
-  () => console.log('Server redirecting on port 3080!'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(express.static('public'));
+if (env === 'production') {
+  const credentials = {
+    cert: fs.readFileSync('./ssl/fullchain.pem'),
+    key: fs.readFileSync('./ssl/privkey.pem')
+  };
 
-http.get("*", function(req, res, next) {
-  res.redirect("https://" + req.headers.host + "/" + req.path);
-});
+  var http = express();
+  http.get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url);
+  });
+  http.listen(3080,
+    () => console.log('Server redirecting on port 3080!'));
 
-https.createServer(credentials, app).listen(PORT,
-  () => console.log('Server listening on port ' + PORT + '!'));
+  app.use(express.static('public'));
+
+  http.get("*", function(req, res, next) {
+    res.redirect("https://" + req.headers.host + "/" + req.path);
+  });
+
+  https.createServer(credentials, app).listen(PORT,
+    () => console.log('Server listening on port ' + PORT + '!'));
+} else {
+  app.listen(PORT,
+    () => console.log('Server listening on port ' + PORT + '!'));
+}
 
 module.exports = app;
